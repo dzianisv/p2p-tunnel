db798b183423d00cd8c4c483fef46f587dd03f54
Dennis <dennis.vashchuk@gmail.com>
2020-04-15 22:49:57 -0700

    Add systemd.service

--- scripts/make-pkg.sh
+++ scripts/make-pkg.sh
@@ -10,8 +10,11 @@ PREFIX="${INSTALL_DIR}/usr"
 
 mkdir -p "${PREFIX}"
 install -d "${PREFIX}/bin"
-install -m -0755  p2p-tunnel "${PREFIX}/bin/"
+install -d "${INSTALL_DIR}/etc/p2p-tunnel"
+install -d "${PREFIX}/lib/systemd/system/"
+install -m 0755  p2p-tunnel "${PREFIX}/bin/"
+install scripts/p2p-tunnel@.service "${PREFIX}/lib/systemd/system"
 
 for TYPE in deb rpm pacman; do
-    fpm -s dir -C "${INSTALL_DIR}" -t "${TYPE}" --name "${NAME}" --version "${VERSION}" --iteration 1 --description "${DESCRIPTION}" .
+    fpm -s dir -C "${INSTALL_DIR}" -a "${ARCH}" -t "${TYPE}" --name "${NAME}" --version "${VERSION}" --iteration 1 --description "${DESCRIPTION}" .
 done
